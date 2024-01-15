67f51b658d872e24dac0f35d3afd33d2ff2dad18
Ganesh Gite <ganeshgite154@gmail.com>
2024-01-16 00:29:56 +0530

    My first commit

---
+++ f2.py
@@ -0,0 +1,1 @@
+I am new lerner, please stay tuned
---
+++ test_function_debug.py
@@ -0,0 +1,24 @@
+a=10
+b=20
+c=a+b
+def f1():
+    print('f1 execution')
+    a=11
+    x=25
+    def f2():
+        print('f2 execution')
+        b=22
+        m=33
+        print(b,m,a,x)    b 
+    print(a,x,b,c)
+    f2()
+def f3():
+    print('f3 execution')
+    p=55
+    q=66
+    return p,q
+f1()
+f3()
+print(a,b,c)
+        
+    
